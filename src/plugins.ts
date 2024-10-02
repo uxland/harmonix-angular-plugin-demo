b0452f0c6ee04e88e684dd99cc104e6809d78baf
Cristian Payet <payet91@gmail.com>
2024-10-02 10:27:29 +0000

    adding pluggin

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,4 +1,4 @@
 import { PluginDefinition, Plugin } from "@uxland/primary-shell";
 
-const importer: () => Promise<Plugin> = () => import("./plugin") as any;
+const importer: () => Promise<Plugin> = () => import("../projects/plugin/src/plugin") as any;
 export const plugins: PluginDefinition[] = [{ pluginId: "angular-plugin", importer: importer}];

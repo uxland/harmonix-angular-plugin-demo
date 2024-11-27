441270a3e5842942cadfafc1d6dede69a0915a5a
Cristian Payet <payet91@gmail.com>
2024-11-27 11:33:26 +0000

    fix: plugins

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,6 +1,6 @@
 import { PluginDefinition, Plugin } from "@uxland/primary-shell";
 
-const importer: () => Promise<Plugin> = () => import("../projects/plugin/src/entry") as any;
+const importer: () => Promise<Plugin> = () => import("../projects/plugin/src/plugin") as any;
 const importer2: () => Promise<Plugin> = () => import("../projects/plugin2/src/public-api") as any;
 export const plugins: PluginDefinition[] = [
     { pluginId: "angular-plugin", importer: importer},

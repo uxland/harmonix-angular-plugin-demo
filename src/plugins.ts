bfbba28d3394956ee67744fb0802bbfd51b0b7c7
Joan Egea <joanegea300@gmail.com>
2025-10-29 11:48:36 +0100

    added clinical monitoring and admin

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,4 +1,11 @@
 import { PluginDefinition, Plugin } from "@uxland/primary-shell";
 
 const importer: () => Promise<Plugin> = () => import("../projects/my-plugin/src/plugin") as any;
-export const plugins: PluginDefinition[] = [{ pluginId: "angular-plugin", importer: importer}]
+const clinicalMonitoringImporter: () => Promise<Plugin> = () => import("../projects/clinical-monitoring/src/plugin") as any;
+const adminClinicalMonitoringImporter: () => Promise<Plugin> = () => import("../projects/admin-clinical-monitoring/src/plugin") as any;
+
+export const plugins: PluginDefinition[] = [
+  { pluginId: "angular-plugin", importer: importer },
+  { pluginId: "clinical-monitoring", importer: clinicalMonitoringImporter },
+  /* { pluginId: "admin-clinical-monitoring", importer: adminClinicalMonitoringImporter }, */
+]

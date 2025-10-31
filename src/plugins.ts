caa7f3bed6a8a3da5b3d55e800015d026371665b
Joan Egea <joanegea300@gmail.com>
2025-10-31 14:03:35 +0100

    remove plugins files and added bundled js

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,8 +1,10 @@
 import { PluginDefinition, Plugin } from "@uxland/primary-shell";
 
 const importer: () => Promise<Plugin> = () => import("../projects/my-plugin/src/plugin") as any;
-const clinicalMonitoringImporter: () => Promise<Plugin> = () => import("../projects/clinical-monitoring/src/plugin") as any;
-const adminClinicalMonitoringImporter: () => Promise<Plugin> = () => import("../projects/admin-clinical-monitoring/src/plugin") as any;
+// @ts-ignore
+const clinicalMonitoringImporter: () => Promise<Plugin> = () => import("./clinical-monitoring.js") as any;
+// @ts-ignore
+const adminClinicalMonitoringImporter: () => Promise<Plugin> = () => import("./admin-clinical-monitoring.js") as any;
 
 export const plugins: PluginDefinition[] = [
   { pluginId: "angular-plugin", importer: importer },
